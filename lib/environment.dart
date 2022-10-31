6c6a87ae0ba02b183672e6208c5865879d7ef1fa
SimonDMaxwell <lolicongress@outlook.com>
2022-10-31 12:06:58 -0500

    API Returned to normal

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,2 +1,1 @@
-// const String apiUrl = 'amber-employ-api.herokuapp.com/api/v1';
-const String apiUrl = 'https://quaco-api.herokuapp.com/api/v1';
+const String apiUrl = 'amber-employ-api.herokuapp.com/api/v1';
