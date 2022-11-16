275489bfdce19c6d7cf215f2adb5842cbe5bf184
SimonDMaxwell <lolicongress@outlook.com>
2022-11-15 22:47:50 -0500

    Added Material to Stabilize MaterialButton in Button1 widget

--- lib/widgets/button1.widget.dart
+++ lib/widgets/button1.widget.dart
@@ -10,14 +10,17 @@ class Button1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
-      clipBehavior: Clip.antiAlias,
-      color: paletteMagenta,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-      onPressed: callback,
-      child: Text(
-        title,
-        style: TextStyle(color: Colors.white, letterSpacing: 4),
+    return Material(
+      color: Colors.transparent,
+      child: MaterialButton(
+        clipBehavior: Clip.antiAlias,
+        color: paletteMagenta,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        onPressed: callback,
+        child: Text(
+          title,
+          style: const TextStyle(color: Colors.white, letterSpacing: 4),
+        ),
       ),
     );
   }
